5bb74d684077b6eb8310018a2dfcad25a1789d64
Jonathan Fischoff <jonathangfischoff@gmail.com>
2023-01-11 17:33:23 -0500

    fix too few

--- scripts/tests/upgrade-too-few-votes.sh
+++ scripts/tests/upgrade-too-few-votes.sh
@@ -5,23 +5,37 @@ baseDir=$thisDir/../
 $baseDir/compile.sh
 $baseDir/generate-datums.sh
 
-
 $baseDir/happy-path/mint-configuration-nft-tx.sh
 $baseDir/wait/until-next-block.sh
-$baseDir/happy-path/lock-upgrade-proposal.sh
-$baseDir/wait/until-next-block.sh
-$baseDir/failure-cases/dummy-tally-lock-too-few-votes.sh
+
+$baseDir/happy-path/lock-treasury.sh
 $baseDir/wait/until-next-block.sh
 
-sleep 30
+$baseDir/minting/mint-vote-0-nft.sh
+$baseDir/wait/until-next-block.sh
+$baseDir/minting/mint-vote-1-nft.sh
+$baseDir/wait/until-next-block.sh
+$baseDir/happy-path/mint-tally-index-nft.sh
+$baseDir/wait/until-next-block.sh
+$baseDir/happy-path/mint-tally-nft.sh
+$baseDir/wait/until-next-block.sh
+$baseDir/happy-path/mint-vote0-long-lived.sh
+$baseDir/wait/until-next-block.sh
+sleep 20
+$baseDir/happy-path/tally-initial-one-vote.sh
+$baseDir/wait/until-next-block.sh
+
+sleep 80
 
 detected=false
 
-"$baseDir/failure-cases/upgrade-configuration-too-few-votes.sh" || {
+"$baseDir/failure-cases/upgrade-configuration-no-relative-majority.sh" || {
     detected=true
 }
 
 if [ $detected == false ]; then
-  echo "Failed to prevent too few votes"
+  echo "Failed to prevent no relative majority"
   exit 1
 fi
+
+echo "Success"
