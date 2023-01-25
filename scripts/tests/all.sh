8de347d50dc5b5fb582270b036f3b37d9283ffc1
Jonathan Fischoff <jonathangfischoff@gmail.com>
2023-01-25 09:56:46 -0800

    update readme and add tests to all tests script

--- scripts/tests/all.sh
+++ scripts/tests/all.sh
@@ -7,6 +7,8 @@ baseDir=$thisDir/../
 $thisDir/vote-mint-and-cancel.sh
 $thisDir/vote-mint-and-count-upgrade.sh
 $thisDir/vote-mint-and-count-general.sh
+$thisDir/vote-mint-and-count-fungible.sh
+$thisDir/vote-mint-and-count-trip.sh
 $thisDir/vote-mint-expired.sh
 $thisDir/upgrade-no-relative-majority.sh
 $thisDir/upgrade-too-few-votes.sh
