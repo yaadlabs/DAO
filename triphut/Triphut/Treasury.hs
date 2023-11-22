818148e8c5892a3c767010e06c7575067faed7bf
nini-faroux <nfarrel1@tcd.ie>
2023-11-22 12:06:21 +0000

    Remove Treasury specific script context and use standard ScriptContext instead

--- triphut/Triphut/Treasury.hs
+++ triphut/Triphut/Treasury.hs
@@ -3,63 +3,13 @@ Module: Triphut.Treasury
 Description: Contains all the treasury specific types.
 -}
 module Triphut.Treasury (
-  -- Script context related types
   TreasuryValidatorConfig (..),
-  TreasuryTxInfo (..),
-  TreasuryTxInInfo (..),
-  TreasuryTxOut (..),
-  TreasuryScriptContext (..),
-  TreasuryScriptPurpose (..),
   Treasury,
 ) where
 
-import Plutus.V1.Ledger.Address (Address)
-import Plutus.V1.Ledger.Crypto (PubKeyHash)
-import Plutus.V1.Ledger.Scripts (Datum, DatumHash)
-import Plutus.V1.Ledger.Time (POSIXTimeRange)
 import Plutus.V1.Ledger.Value as V
-import Plutus.V2.Ledger.Tx hiding (Mint)
-import PlutusTx (
-  makeIsDataIndexed,
-  makeLift,
-  unstableMakeIsData,
- )
-import PlutusTx.AssocMap (Map)
+import PlutusTx (makeLift)
 import PlutusTx.Prelude (BuiltinData)
-
-data TreasuryTxOut = TreasuryTxOut
-  { tTxOutAddress :: Address
-  , tTxOutValue :: Value
-  , tTxOutDatum :: OutputDatum
-  , tTxOutReferenceScript :: BuiltinData
-  }
-
-data TreasuryTxInInfo = TreasuryTxInInfo
-  { tTxInInfoOutRef :: TxOutRef
-  , tTxInInfoResolved :: TreasuryTxOut
-  }
-
-newtype TreasuryScriptPurpose = TreasurySpend TxOutRef
-
-data TreasuryScriptContext = TreasuryScriptContext
-  { tScriptContextTxInfo :: TreasuryTxInfo
-  , tScriptContextPurpose :: TreasuryScriptPurpose
-  }
-
-data TreasuryTxInfo = TreasuryTxInfo
-  { tTxInfoInputs :: [TreasuryTxInInfo]
-  , tTxInfoReferenceInputs :: [TreasuryTxInInfo]
-  , tTxInfoOutputs :: [TreasuryTxOut]
-  , tTxInfoFee :: BuiltinData
-  , tTxInfoMint :: Value
-  , tTxInfoDCert :: BuiltinData
-  , tTxInfoWdrl :: BuiltinData
-  , tTxInfoValidRange :: POSIXTimeRange
-  , tTxInfoSignatories :: [PubKeyHash]
-  , tTxInfoRedeemers :: BuiltinData
-  , tTxInfoData :: Map DatumHash Datum
-  , tTxInfoId :: BuiltinData
-  }
 
 type Treasury = BuiltinData
 
@@ -68,9 +18,4 @@ data TreasuryValidatorConfig = TreasuryValidatorConfig
   , tvcConfigNftTokenName :: TokenName
   }
 
-unstableMakeIsData ''TreasuryTxOut
-unstableMakeIsData ''TreasuryTxInInfo
-makeIsDataIndexed ''TreasuryScriptPurpose [('TreasurySpend, 1)]
-unstableMakeIsData ''TreasuryScriptContext
-unstableMakeIsData ''TreasuryTxInfo
 makeLift ''TreasuryValidatorConfig
