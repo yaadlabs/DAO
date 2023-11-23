e5a700abf1ef39e8ad1e0eeff2654556695a624d
nini-faroux <nfarrel1@tcd.ie>
2023-11-23 10:43:07 +0000

    Change names from triphut to dao

---
+++ dao-specs/Spec/SampleData.hs
@@ -0,0 +1,167 @@
+module Spec.SampleData (
+  sampleDynamicConfig,
+  sampleVoteMinterDynamicConfig,
+  sampleVoteDynamicConfig,
+  sampleTallyDynamicConfig,
+  sampleHighRelativeMajorityHighTotalVotesDynamicConfig,
+) where
+
+import Dao.Tally (TallyDynamicConfigDatum (..))
+import Dao.Types (DynamicConfigDatum (..))
+import Dao.Vote (
+  VoteDynamicConfigDatum (..),
+  VoteMinterDynamicConfigDatum (..),
+ )
+import Plutus.V1.Ledger.Value (CurrencySymbol, TokenName (TokenName), adaSymbol, adaToken)
+import Plutus.V2.Ledger.Api (ValidatorHash (ValidatorHash))
+import PlutusTx (toBuiltinData)
+import PlutusTx.Prelude (Integer)
+import Spec.Tally.Script (tallyValidatorHash')
+import Spec.Values (dummyTallySymbol, dummyVoteFungibleSymbol)
+import Spec.Vote.SampleData (sampleVoteMinterConfig)
+import Spec.Vote.Script (voteCurrencySymbol, voteValidatorHash')
+
+-- DynamicConfigDatum samples
+sampleDynamicConfig :: DynamicConfigDatum
+sampleDynamicConfig =
+  DynamicConfigDatum
+    { dcTallyNft = dummyTallySymbol
+    , dcTallyValidator = tallyValidatorHash'
+    , dcTreasuryValidator = ValidatorHash ""
+    , dcConfigurationValidator = ValidatorHash ""
+    , dcVoteCurrencySymbol = adaSymbol
+    , dcVoteTokenName = adaToken
+    , dcVoteValidator = ValidatorHash ""
+    , dcUpgradeMajorityPercent = 1
+    , dcUpgradeRelativeMajorityPercent = 1
+    , dcGeneralMajorityPercent = 1
+    , dcGeneralRelativeMajorityPercent = 20
+    , dcTripMajorityPercent = 1
+    , dcTripRelativeMajorityPercent = 1
+    , dcVoteNft = adaSymbol
+    , dcVoteFungibleCurrencySymbol = adaSymbol
+    , dcVoteFungibleTokenName = adaToken
+    , dcTotalVotes = 1
+    , dcProposalTallyEndOffset = 0
+    , dcMaxGeneralDisbursement = 1
+    , dcMaxTripDisbursement = 1
+    , dcAgentDisbursementPercent = 1
+    , dcFungibleVotePercent = 1
+    , dcTallyIndexNft = adaSymbol
+    }
+
+sampleHighRelativeMajorityHighTotalVotesDynamicConfig :: DynamicConfigDatum
+sampleHighRelativeMajorityHighTotalVotesDynamicConfig =
+  DynamicConfigDatum
+    { dcTallyNft = dummyTallySymbol
+    , dcTallyValidator = tallyValidatorHash'
+    , dcTreasuryValidator = ValidatorHash ""
+    , dcConfigurationValidator = ValidatorHash ""
+    , dcVoteCurrencySymbol = adaSymbol
+    , dcVoteTokenName = adaToken
+    , dcVoteValidator = ValidatorHash ""
+    , dcUpgradeMajorityPercent = 1
+    , dcUpgradeRelativeMajorityPercent = 70
+    , -- \^ Just set to high value for negative test for upgrading config
+      dcGeneralMajorityPercent = 1
+    , dcGeneralRelativeMajorityPercent = 1
+    , dcTripMajorityPercent = 1
+    , dcTripRelativeMajorityPercent = 1
+    , dcVoteNft = adaSymbol
+    , dcVoteFungibleCurrencySymbol = adaSymbol
+    , dcVoteFungibleTokenName = adaToken
+    , dcTotalVotes = 2000
+    , -- \^ Set it high for negative test for upgrading config
+      dcProposalTallyEndOffset = 0
+    , dcMaxGeneralDisbursement = 1
+    , dcMaxTripDisbursement = 1
+    , dcAgentDisbursementPercent = 1
+    , dcFungibleVotePercent = 1
+    , dcTallyIndexNft = adaSymbol
+    }
+
+-- VoteMinterDynamicConfigDatum samples
+sampleVoteMinterDynamicConfig :: VoteMinterDynamicConfigDatum
+sampleVoteMinterDynamicConfig =
+  VoteMinterDynamicConfigDatum
+    { vmdcTallyIndexNft = toBuiltinData (adaSymbol :: CurrencySymbol)
+    , vmdcTallyNft = dummyTallySymbol
+    , vmdcTallyValidator = toBuiltinData tallyValidatorHash'
+    , vmdcTreasuryValidator = toBuiltinData (ValidatorHash "")
+    , vmdcConfigurationValidator = toBuiltinData (ValidatorHash "")
+    , vmdcVoteCurrencySymbol = toBuiltinData (adaSymbol :: CurrencySymbol)
+    , vmdcVoteTokenName = TokenName "vote"
+    , vmdcVoteValidator = voteValidatorHash'
+    , vmdcUpgradeMajorityPercent = toBuiltinData (1 :: Integer)
+    , vmdcUpgradRelativeMajorityPercent = toBuiltinData (1 :: Integer)
+    , vmdcGeneralMajorityPercent = toBuiltinData (1 :: Integer)
+    , vmdcGeneralRelativeMajorityPercent = toBuiltinData (1 :: Integer)
+    , vmdcTripMajorityPercent = toBuiltinData (1 :: Integer)
+    , vmdcTripRelativeMajorityPercent = toBuiltinData (1 :: Integer)
+    , vmdcTotalVotes = toBuiltinData (1 :: Integer)
+    , vmdcVoteNft = voteCurrencySymbol sampleVoteMinterConfig
+    , vmdcVoteFungibleCurrencySymbol = toBuiltinData (adaSymbol :: CurrencySymbol)
+    , vmdcVoteFungibleTokenName = toBuiltinData (adaToken :: TokenName)
+    , vmdcProposalTallyEndOffset = toBuiltinData (1 :: Integer)
+    , vmdcMaxGeneralDisbursement = toBuiltinData (1 :: Integer)
+    , vmdcMaxTripDisbursement = toBuiltinData (1 :: Integer)
+    , vmdcAgentDisbursementPercent = toBuiltinData (1 :: Integer)
+    , vmdcFungibleVotePercent = toBuiltinData (1 :: Integer)
+    }
+
+-- VoteDynamicConfigDatum samples
+sampleVoteDynamicConfig :: VoteDynamicConfigDatum
+sampleVoteDynamicConfig =
+  VoteDynamicConfigDatum
+    { vdcTallyIndexNft = toBuiltinData (adaSymbol :: CurrencySymbol)
+    , vdcTallyNft = toBuiltinData (adaSymbol :: CurrencySymbol)
+    , vdcTallyValidator = toBuiltinData tallyValidatorHash'
+    , vdcTreasuryValidator = toBuiltinData (ValidatorHash "")
+    , vdcConfigurationValidator = toBuiltinData (ValidatorHash "")
+    , vdcVoteCurrencySymbol = toBuiltinData (dummyVoteFungibleSymbol :: CurrencySymbol)
+    , vdcVoteTokenName = toBuiltinData (adaToken :: TokenName)
+    , vdcVoteValidator = toBuiltinData (ValidatorHash "")
+    , vdcUpgradeMajorityPercent = toBuiltinData (1 :: Integer)
+    , vdcUpgradRelativeMajorityPercent = toBuiltinData (1 :: Integer)
+    , vdcGeneralMajorityPercent = toBuiltinData (1 :: Integer)
+    , vdcGeneralRelativeMajorityPercent = toBuiltinData (1 :: Integer)
+    , vdcTripMajorityPercent = toBuiltinData (1 :: Integer)
+    , vdcTripRelativeMajorityPercent = toBuiltinData (1 :: Integer)
+    , vdcTotalVotes = toBuiltinData (1 :: Integer)
+    , vdcVoteNft = toBuiltinData adaSymbol
+    , vdcVoteFungibleCurrencySymbol = toBuiltinData (adaSymbol :: CurrencySymbol)
+    , vdcVoteFungibleTokenName = toBuiltinData (adaToken :: TokenName)
+    , vdcProposalTallyEndOffset = toBuiltinData (1 :: Integer)
+    , vdcMaxGeneralDisbursement = toBuiltinData (1 :: Integer)
+    , vdcMaxTripDisbursement = toBuiltinData (1 :: Integer)
+    , vdcAgentDisbursementPercent = toBuiltinData (1 :: Integer)
+    , vdcFungibleVotePercent = toBuiltinData (1 :: Integer)
+    }
+
+sampleTallyDynamicConfig :: TallyDynamicConfigDatum
+sampleTallyDynamicConfig =
+  TallyDynamicConfigDatum
+    { tdcTallyIndexNft = toBuiltinData (adaSymbol :: CurrencySymbol)
+    , tdcTallyNft = dummyTallySymbol
+    , tdcTallyValidator = toBuiltinData tallyValidatorHash'
+    , tdcTreasuryValidator = toBuiltinData (ValidatorHash "")
+    , tdcConfigurationValidator = toBuiltinData (ValidatorHash "")
+    , tdcVoteCurrencySymbol = adaSymbol
+    , tdcVoteTokenName = toBuiltinData (TokenName "")
+    , tdcVoteValidator = voteValidatorHash'
+    , tdcUpgradeMajorityPercent = toBuiltinData (1 :: Integer)
+    , tdcUpgradRelativeMajorityPercent = toBuiltinData (1 :: Integer)
+    , tdcGeneralMajorityPercent = toBuiltinData (1 :: Integer)
+    , tdcGeneralRelativeMajorityPercent = toBuiltinData (1 :: Integer)
+    , tdcTripMajorityPercent = toBuiltinData (1 :: Integer)
+    , tdcTripRelativeMajorityPercent = toBuiltinData (1 :: Integer)
+    , tdcTotalVotes = toBuiltinData (1 :: Integer)
+    , tdcVoteNft = voteCurrencySymbol sampleVoteMinterConfig
+    , tdcVoteFungibleCurrencySymbol = dummyVoteFungibleSymbol
+    , tdcVoteFungibleTokenName = adaToken
+    , tdcProposalTallyEndOffset = toBuiltinData (1 :: Integer)
+    , tdcMaxGeneralDisbursement = toBuiltinData (1 :: Integer)
+    , tdcMaxTripDisbursement = toBuiltinData (1 :: Integer)
+    , tdcAgentDisbursementPercent = toBuiltinData (1 :: Integer)
+    , tdcFungibleVotePercent = 1
+    }
---
+++ dao/Dao/Vote.hs
@@ -0,0 +1,250 @@
+{- |
+Module: Dao.Vote
+Description: Contains all the voting specific types.
+-}
+module Dao.Vote (
+  -- * Datums
+  VoteDatum (..),
+  VoteMinterDynamicConfigDatum (..),
+  VoteDynamicConfigDatum (..),
+
+  -- * Redeemers
+  VoteMinterActionRedeemer (..),
+  VoteActionRedeemer (..),
+
+  -- * General vote related types
+  VoteDirection (..),
+
+  -- * Script arguments, containing relevant CurrenySymbol and TokenName
+  VoteMinterConfig (..),
+  VoteValidatorConfig (..),
+  -- Script context related types
+  VoteScriptContext (..),
+  VoteTxOut (..),
+  VoteTxInInfo (..),
+  VoteTxInfo (..),
+  VoteMinterTxOut (..),
+  VoteMinterTxInfo (..),
+  VoteMinterTxInInfo (..),
+  VoteMinterScriptContext (..),
+  VoteMinterScriptPurpose (..),
+  VoteAddress (..),
+  VoteMinterAddress (..),
+) where
+
+import Plutus.V1.Ledger.Address (Address)
+import Plutus.V1.Ledger.Credential (Credential)
+import Plutus.V1.Ledger.Scripts (
+  Datum,
+  DatumHash,
+  ValidatorHash,
+ )
+import Plutus.V1.Ledger.Value (
+  CurrencySymbol,
+  TokenName,
+  Value,
+ )
+import Plutus.V2.Ledger.Tx hiding (Mint)
+import PlutusTx (makeLift, unstableMakeIsData)
+import PlutusTx.AssocMap (Map)
+import PlutusTx.Prelude (
+  Bool (False, True),
+  BuiltinData,
+  Integer,
+  (==),
+ )
+import PlutusTx.Prelude qualified as PlutusTx
+
+-- | 'Dao.Vote.Script.mkVoteMinter' argument
+data VoteMinterConfig = VoteMinterConfig
+  { vmcConfigNftCurrencySymbol :: CurrencySymbol
+  , vmcConfigNftTokenName :: TokenName
+  }
+
+makeLift ''VoteMinterConfig
+
+-- | Vote direction
+data VoteDirection = For | Against
+
+instance PlutusTx.Eq VoteDirection where
+  For == For = True
+  Against == Against = True
+  _ == _ = False
+
+-- | The vote datum, represnting a vote cast by a user on a specific proposal
+data VoteDatum = VoteDatum
+  { vProposalTokenName :: TokenName
+  -- ^ The name of the proposal for which this vote relates to.
+  -- This is checked in 'Dao.Tally.Script.validateTally' to ensure
+  -- the vote is for the correct proposal.
+  , vDirection :: VoteDirection
+  -- ^ Whether the vote is for or against the proposal.
+  , vOwner :: Address
+  -- ^ The address of the user casting the vote.
+  , vReturnAda :: Integer
+  -- ^ Ada amount to return
+  }
+
+-- | Redeemer for 'Dao.Vote.Script.mkVoteMinter' policy
+data VoteMinterActionRedeemer = Mint | Burn
+
+unstableMakeIsData ''VoteDirection
+unstableMakeIsData ''VoteMinterActionRedeemer
+unstableMakeIsData ''VoteDatum
+
+data VoteMinterTxOut = VoteMinterTxOut
+  { vmTxOutAddress :: VoteMinterAddress
+  , vmTxOutValue :: Value
+  , vmTxOutDatum :: OutputDatum
+  , vmTxOutReferenceScript :: BuiltinData
+  }
+
+data VoteMinterAddress = VoteMinterAddress
+  { vmAddressCredential :: Credential
+  , vmAddressStakingCredential :: BuiltinData
+  }
+
+data VoteMinterTxInInfo = VoteMinterTxInInfo
+  { vmTxInInfoOutRef :: BuiltinData
+  , vmTxInInfoResolved :: VoteMinterTxOut
+  }
+
+newtype VoteMinterScriptPurpose = VMMinting CurrencySymbol
+
+data VoteMinterScriptContext = VoteMinterScriptContext
+  { vmScriptContextTxInfo :: VoteMinterTxInfo
+  , vmScriptContextPurpose :: VoteMinterScriptPurpose
+  }
+
+data VoteMinterTxInfo = VoteMinterTxInfo
+  { vmTxInfoInputs :: BuiltinData
+  , vmTxInfoReferenceInputs :: BuiltinData
+  , vmTxInfoOutputs :: BuiltinData
+  , vmTxInfoFee :: BuiltinData
+  , vmTxInfoMint :: Value
+  , vmTxInfoDCert :: BuiltinData
+  , vmTxInfoWdrl :: BuiltinData
+  , vmTxInfoValidRange :: BuiltinData
+  , vmTxInfoSignatories :: BuiltinData
+  , vmTxInfoRedeemers :: BuiltinData
+  , vmTxInfoData :: BuiltinData
+  , vmTxInfoId :: BuiltinData
+  }
+
+-- | Vote minter config datum, representation mirrors the main 'Dao.Types.DynamicConfigDatum'
+data VoteMinterDynamicConfigDatum = VoteMinterDynamicConfigDatum
+  { vmdcTallyIndexNft :: BuiltinData
+  , vmdcTallyNft :: CurrencySymbol
+  , vmdcTallyValidator :: BuiltinData
+  , vmdcTreasuryValidator :: BuiltinData
+  , vmdcConfigurationValidator :: BuiltinData
+  , vmdcVoteCurrencySymbol :: BuiltinData
+  , vmdcVoteTokenName :: TokenName
+  , vmdcVoteValidator :: ValidatorHash
+  , vmdcUpgradeMajorityPercent :: BuiltinData
+  , vmdcUpgradRelativeMajorityPercent :: BuiltinData
+  , vmdcGeneralMajorityPercent :: BuiltinData
+  , vmdcGeneralRelativeMajorityPercent :: BuiltinData
+  , vmdcTripMajorityPercent :: BuiltinData
+  , vmdcTripRelativeMajorityPercent :: BuiltinData
+  , vmdcTotalVotes :: BuiltinData
+  , vmdcVoteNft :: CurrencySymbol
+  , vmdcVoteFungibleCurrencySymbol :: BuiltinData
+  , vmdcVoteFungibleTokenName :: BuiltinData
+  , vmdcProposalTallyEndOffset :: BuiltinData
+  , vmdcMaxGeneralDisbursement :: BuiltinData
+  , vmdcMaxTripDisbursement :: BuiltinData
+  , vmdcAgentDisbursementPercent :: BuiltinData
+  , vmdcFungibleVotePercent :: BuiltinData
+  }
+
+-- | Vote config datum, representation mirrors the main 'Dao.Types.DynamicConfigDatum'
+data VoteDynamicConfigDatum = VoteDynamicConfigDatum
+  { vdcTallyIndexNft :: BuiltinData
+  , vdcTallyNft :: BuiltinData
+  , vdcTallyValidator :: BuiltinData
+  , vdcTreasuryValidator :: BuiltinData
+  , vdcConfigurationValidator :: BuiltinData
+  , vdcVoteCurrencySymbol :: BuiltinData
+  , vdcVoteTokenName :: BuiltinData
+  , vdcVoteValidator :: BuiltinData
+  , vdcUpgradeMajorityPercent :: BuiltinData
+  , vdcUpgradRelativeMajorityPercent :: BuiltinData
+  , vdcGeneralMajorityPercent :: BuiltinData
+  , vdcGeneralRelativeMajorityPercent :: BuiltinData
+  , vdcTripMajorityPercent :: BuiltinData
+  , vdcTripRelativeMajorityPercent :: BuiltinData
+  , vdcTotalVotes :: BuiltinData
+  , vdcVoteNft :: BuiltinData
+  , vdcVoteFungibleCurrencySymbol :: BuiltinData
+  , vdcVoteFungibleTokenName :: BuiltinData
+  , vdcProposalTallyEndOffset :: BuiltinData
+  , vdcMaxGeneralDisbursement :: BuiltinData
+  , vdcMaxTripDisbursement :: BuiltinData
+  , vdcAgentDisbursementPercent :: BuiltinData
+  , vdcFungibleVotePercent :: BuiltinData
+  }
+
+unstableMakeIsData ''VoteMinterAddress
+unstableMakeIsData ''VoteMinterTxOut
+unstableMakeIsData ''VoteMinterTxInInfo
+unstableMakeIsData ''VoteMinterScriptPurpose
+unstableMakeIsData ''VoteMinterScriptContext
+unstableMakeIsData ''VoteMinterTxInfo
+unstableMakeIsData ''VoteMinterDynamicConfigDatum
+
+data VoteAddress = VoteAddress
+  { vAddressCredential :: Credential
+  , vAddressStakingCredential :: BuiltinData
+  }
+
+data VoteTxOut = VoteTxOut
+  { vTxOutAddress :: VoteAddress
+  , vTxOutValue :: Value
+  , vTxOutDatum :: OutputDatum
+  , vTxOutReferenceScript :: BuiltinData
+  }
+
+data VoteTxInInfo = VoteTxInInfo
+  { vTxInInfoOutRef :: TxOutRef
+  , vTxInInfoResolved :: VoteTxOut
+  }
+
+data VoteScriptContext = VoteScriptContext
+  { vScriptContextTxInfo :: VoteTxInfo
+  , vScriptContextPurpose :: BuiltinData
+  }
+
+data VoteTxInfo = VoteTxInfo
+  { vTxInfoInputs :: BuiltinData
+  , vTxInfoReferenceInputs :: [VoteTxInInfo]
+  , vTxInfoOutputs :: BuiltinData
+  , vTxInfoFee :: BuiltinData
+  , vTxInfoMint :: BuiltinData
+  , vTxInfoDCert :: BuiltinData
+  , vTxInfoWdrl :: BuiltinData
+  , vTxInfoValidRange :: BuiltinData
+  , vTxInfoSignatories :: BuiltinData
+  , vTxInfoRedeemers :: BuiltinData
+  , vTxInfoData :: Map DatumHash Datum
+  , vTxInfoId :: BuiltinData
+  }
+
+-- | Redeemer for 'Dao.Vote.Script.validateVote' validator
+data VoteActionRedeemer
+  = Count
+  | Cancel
+
+data VoteValidatorConfig = VoteValidatorConfig
+  { vvcConfigNftCurrencySymbol :: CurrencySymbol
+  , vvcConfigNftTokenName :: TokenName
+  }
+
+unstableMakeIsData ''VoteAddress
+unstableMakeIsData ''VoteTxOut
+unstableMakeIsData ''VoteTxInInfo
+unstableMakeIsData ''VoteScriptContext
+unstableMakeIsData ''VoteTxInfo
+unstableMakeIsData ''VoteActionRedeemer
+unstableMakeIsData ''VoteDynamicConfigDatum
+makeLift ''VoteValidatorConfig
